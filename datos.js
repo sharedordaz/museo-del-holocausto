7435eff13bdf440d3786f57e767cff7266778fce
sharedordaz <sharedordaz@live.com.mx>
2022-01-17 18:25:28 -0600

    added autor ibime

--- datos.js
+++ datos.js
@@ -11,7 +11,10 @@ fetch(requestURL)
     //console.table(jsonObject);  // temporary checking for valid response and data parsing'
     const obras = jsonObject['main'];
 
+    let index = 0;
+
     for (let i = 0; i < obras.length; i++ ) {
+      if (i <= 7){
         let image = document.createElement("img");
 
         //let h2 = document.createElement('h2');       
@@ -20,34 +23,48 @@ fetch(requestURL)
         // c_info.textContent = obras[i].contact_info;
 
         image.addEventListener("click", () => {
-        let click_info = document.querySelector(".click-info");
-        //console.log(click_info);
-          click_info.style.display = "block";
+          let click_info = document.querySelector(".click-info");
+          //console.log(click_info);
+            click_info.style.display = "block";
+            
+          let title = document.createElement("h2");
+          title.textContent = obras[i].nombre;
+          click_info.appendChild(title);
+
+          let inner_image = document.createElement("img");
+          inner_image.setAttribute("src", obras[i].imagen);
+          click_info.appendChild(inner_image);
+
+          let autor = document.createElement("p");
+          autor.innerHTML = "<b>Autor: </b>" + obras[i].autor;
+          click_info.appendChild(autor);
+
+          if (obras[i].ibime == true){
+            let autoribime = document.createElement("b");
+            autoribime.textContent = "(Autor Ibime)";
+            autoribime.style.color = "blue";
+            click_info.appendChild(autoribime);
+          };
+          let tecnica = document.createElement("p");
+          tecnica.innerHTML = "<b>Tecnica: </b>" + obras[i].tecnica;
+          click_info.appendChild(tecnica);
+
+          let descripcion = document.createElement("p");
+          descripcion.textContent =  obras[i].descripcion;
+          click_info.appendChild(descripcion);
+
+          let generacion = document.createElement("p");
+          generacion.innerHTML = "<b>De la generacion: </b>" + obras[i].gen;
+          click_info.appendChild(generacion);
+
+
+          let site_title = document.getElementById("title");
           
-        let title = document.createElement("h2");
-        title.textContent = obras[i].nombre;
-        click_info.appendChild(title);
+          let logo = document.getElementById("ibime-logo");
 
-        let inner_image = document.createElement("img");
-        inner_image.setAttribute("src", obras[i].imagen);
-        click_info.appendChild(inner_image);
-
-        let autor = document.createElement("p");
-        autor.innerHTML = "<b>Autor: </b>" + obras[i].autor;
-        click_info.appendChild(autor);
-
-        let tecnica = document.createElement("p");
-        tecnica.innerHTML = "<b>Tecnica: </b>" + obras[i].tecnica;
-        click_info.appendChild(tecnica);
-
-        let descripcion = document.createElement("p");
-        descripcion.textContent =  obras[i].descripcion;
-        click_info.appendChild(descripcion);
-
-        let generacion = document.createElement("p");
-        generacion.innerHTML = "<b>De la generacion: </b>" + obras[i].gen;
-        click_info.appendChild(generacion);
-
+          site_title.style.display = "none";
+          logo.style.display = "none";
+        
         }); 
 
 
@@ -56,6 +73,10 @@ fetch(requestURL)
         // card.appendChild(h2);
         
         grid.appendChild(image);
+      }
+        if (i == 7){
+          
+        }
     }
   });
 
